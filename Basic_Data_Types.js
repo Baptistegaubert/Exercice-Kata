97a6f95b7aa2204ca713ad387357f67af7ec61fa
baptiste gaubert <91956738+Baptistegaubert@users.noreply.github.com>
2022-02-03 14:56:37 +0100

    Add files via upload

---
+++ Basic_Data_Types.js
@@ -0,0 +1,6 @@
+function trueOrFalse(val){
+    if (val==false)  return "false"
+    if (val==null) return "false"
+    if (val==undefined) return "false";             
+    else return "true";
+  }
